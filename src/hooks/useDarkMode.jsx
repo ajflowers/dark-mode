8d63551a4df52a227ad58796589bb1a4a98f2cf2
Angela Flowers <me@angela.flowers>
2019-09-04 21:42:30 -0400

    restored line color change on dark mode toggle

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -6,16 +6,16 @@ const useDarkMode = initialValue => {
 
     useEffect(() => {
         document.body.className = displayMode ? 'dark-mode' : ''
-        // const chartLines = document.getElementsByClassName('recharts-line-curve')
-        // // console.log(chartLines)
-        // Array.prototype.forEach.call(chartLines, function(line) {
-        //     line.setAttribute('stroke', displayMode ? '#f68819' : '#8884d8')
-        // })
-        // const chartDots = document.getElementsByClassName('recharts-line-dot')
-        // // console.log(chartDots)
-        // Array.prototype.forEach.call(chartDots, function(dot) {
-        //     dot.setAttribute('stroke', displayMode ? '#f68819' : '#8884d8')
-        // })
+        const chartLines = document.getElementsByClassName('recharts-line-curve')
+        // console.log(chartLines)
+        Array.prototype.forEach.call(chartLines, function(line) {
+            line.setAttribute('stroke', displayMode ? '#f68819' : '#8884d8')
+        })
+        const chartDots = document.getElementsByClassName('recharts-line-dot')
+        // console.log(chartDots)
+        Array.prototype.forEach.call(chartDots, function(dot) {
+            dot.setAttribute('stroke', displayMode ? '#f68819' : '#8884d8')
+        })
     }, [displayMode])
 
     return [displayMode, setDisplayMode]
